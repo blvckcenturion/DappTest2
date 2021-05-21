0b0151f2636ee9c33c3ec6daadbb71a12a64a8e8
Santiago Sarabia <santiagosarabia@Santiagos-Mac-mini.local>
2021-05-21 19:59:32 -0400

    First commit

---
+++ hardhat.config.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.3",
+  paths: {
+    artifacts: './src/artifacts'
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/80898d9e292c4167be9de1173fa340e5",
+      accounts: [`0x${process.env.ACCOUNT_KEY}`]
+    }
+  }
+};
+
